43490b5c66afea6f1cf9ce8a6515047d3099eb64
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-05-05 21:43:53 +0200

    add delete authtoken 1h

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
 import Layout from "./Layout/layout";
 
 function App() {
+  useEffect(() => {
+    const oneHourInMs = 60 * 60 * 1000;
+
+    // Define a function to remove data after one hour
+    const removeLocalStorageAfterOneHour = () => {
+      localStorage.removeItem("token");
+    };
+
+    // Set a timer to call the removal function after one hour
+    const timer = setTimeout(removeLocalStorageAfterOneHour, oneHourInMs);
+
+    // Clean up the timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="">
       <Layout />
     </div>
   );
 }
+
 export default App;
